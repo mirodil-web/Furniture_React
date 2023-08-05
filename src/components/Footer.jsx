be92b76f93ea53a79d86363df7bb2960b99e3210
tolamirzayev-077 <mirodiltolamirzayev5@gmail.com>
2023-08-05 11:50:52 +0500

    footer img add, range section img add and style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,26 @@ import React from 'react'
 
 const Footer = () => {
   return (
-      <footer className='bg-darkblue py-12'>
-          <div className="container">
-              <div>
-                  <h2>Beauty Care</h2>
-                  <p>Do eiusmod tempor incididunt ut labore etdolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
-                  <h3>Follow Us</h3>
+      <footer className='bg-darkblue py-12 font-montserrat'>
+          <div className="container flex justify-between items-center">
+              
+              <div className='max-w-[400px]'>
+                  <h2 className='text-white font-bold text-2xl mb-8'>Beauty Care</h2>
+                  <p className='font-normal  leading-9 text-white mb-5'>Do eiusmod tempor incididunt ut labore etdolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+                  <h3 className='text-white font-bold text-2xl '>Follow Us</h3>
               </div>
+
+              <div>
+                  <h3 className='text-white font-bold text-2xl mb-6'>Instagram Shop</h3>
+                  <div className='flex space-x-5'>
+                      <img className='rounded-sm' width={175} height={185} src="../../public/assets/IG-1.jpg" alt="img" />
+                      <img className='rounded-sm' width={175} height={185} src="../../public/assets/IG-2.jpg" alt="img" />
+                      <img className='rounded-sm' width={175} height={185} src="../../public/assets/IG-3.jpg" alt="img" />
+                      <img className='rounded-sm' width={175} height={185} src="../../public/assets/IG-4.jpg" alt="img" />
+
+                  </div>
+              </div>
+
           </div>
     </footer>
   )
--- src/components/Rangesection.jsx
+++ src/components/Rangesection.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
 
 const Rangesection = () => {
-  return (
-      <div className='container'>
-          <section className='py-14 font-montserrat'>
-              <div className='text-center mb-12'>
-                  <h2 className='text-4xl font-bold mb-5'>Browse The Range</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                  <div className='flex justify-between'>
-                      <div>
-                          <img src="" alt="Home" />
-                          <h4></h4>
-                      </div>
-                  </div>
-              </div>
-          </section>
-    </div>
-  )
+    return (
+        <div className='container'>
+            <section className='py-14 font-montserrat'>
+                <div className='text-center mb-12'>
+                    <h2 className='text-4xl font-bold mb-5'>Browse The Range</h2>
+                    <p className='mb-9'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <div className='flex justify-between'>
+                        <div>
+                            <img className='mb-8' src="../../public/assets/Dining.jpg" alt="Home" />
+                            <h4 className='text-2xl font-semibold text-gblack'>Dining</h4>
+                        </div>
+                        <div>
+                            <img className='mb-8' src="../../public/assets/Image-living.jpg" alt="Home" />
+                            <h4 className='text-2xl font-semibold text-gblack'>Living</h4>
+                        </div>
+                        <div>
+                            <img className='mb-8' src="../../public/assets/Beadroom.jpg" alt="Home" />
+                            <h4 className='text-2xl font-semibold text-gblack'>Living</h4>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default Rangesection
